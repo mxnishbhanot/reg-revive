c9189b193310bf5485a9f912a5f9c7c99c148322
Manish Kumar <manish.kumar@commitbit.com>
2025-04-15 07:57:34 -0500

    added bookign modal

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
+import { BookingModalComponent } from '../shared/booking-modal/booking-modal.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [BookingModalComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   menuOpen = false;
+  showModal = false;
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
   }
 }
---
+++ src/app/components/shared/booking-modal/booking-modal.component.ts
@@ -0,0 +1,61 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-booking-modal',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './booking-modal.component.html',
+  styleUrl: './booking-modal.component.scss'
+})
+export class BookingModalComponent {
+  @Input() show = false;
+  @Output() close = new EventEmitter<void>();
+
+  bookingData = {
+    fullName: '',
+    email: '',
+    phone: '',
+    serviceType: '',
+    // date: '',
+    // time: ''
+  };
+
+  formSubmitted = false;
+
+  submitBooking() {
+    this.formSubmitted = true;
+    
+    // Form validation would go here
+    if (this.bookingData.fullName && this.bookingData.email && 
+        this.bookingData.phone && this.bookingData.serviceType) {
+      console.log('Booking submitted:', this.bookingData);
+      
+      // Reset form
+      setTimeout(() => {
+        this.formSubmitted = false;
+        this.resetForm();
+        this.close.emit();
+      }, 1500);
+    }
+  }
+
+  validateModal() {
+    if (this.bookingData.fullName.trim().length === 0 || this.bookingData.email.trim().length === 0 ||
+      this.bookingData.phone.trim().length === 0 || this.bookingData.serviceType.trim().length === 0) {
+      return false
+    }
+    return true;
+  }
+
+  resetForm() {
+    this.bookingData = {
+      fullName: '',
+      email: '',
+      phone: '',
+      serviceType: '',
+      // date: '',
+      // time: ''
+    };
+  }
+}
